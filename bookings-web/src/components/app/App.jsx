9c8e9564a04ed109d7ff4567d92532eea5c16d4d
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-05-23 17:34:13 -0700

    initial configs and router setup

--- bookings-web/src/components/app/App.jsx
+++ bookings-web/src/components/app/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Switch, Route } from 'react-router-dom';
 import Getaways from '../../containers/Getaways';
+import Header from './Header';
 
 export default function App() {
-  return <Getaways />;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Switch>
+        <Route path="/" component={Getaways} />
+      </Switch>
+    </>
+  );
 }
